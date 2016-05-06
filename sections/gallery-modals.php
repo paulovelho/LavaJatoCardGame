1341481a6dc47da4ad652b10c3ea611dda1d5727
Paulo Henrique Martins <paulo.martins@savedo.de>
2016-05-06 12:20:33 +0200

    carta juiz

--- sections/gallery-modals.php
+++ sections/gallery-modals.php
@@ -13,7 +13,7 @@
                     <div class="modal-body">
                         <!-- Project Details Go Here -->
                         <h2>Identidade visual</h2>
-                        <p class="item-intro text-muted">Para agradar coxinhas e petralhas.</p>
+                        <p class="item-intro text-muted">Para agradar coxinhas e petralhas</p>
                         <p>Identidade visual elegante e moderna, mantendo a seriedade que o momento político merece com a fanfarronice que o sistema jurídico proporciona.</p>
                         <p>
                             Tão moderno que é ideal pra ser jogado em uma festa de casamento.<br/>
@@ -22,6 +22,35 @@
                         </p>
                         <img class="img-responsive img-centered" src="images/gallery/back-cards.png" alt="">
                         <p>Você vai querer emoldurar o jogo e colocar na sua parede.</p>
+                        <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Fechar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="portfolio-modal modal fade" id="arteJuiz" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+                <div class="rl">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="modal-body">
+                        <!-- Project Details Go Here -->
+                        <h2>Artisticamente foda</h2>
+                        <p class="item-intro text-muted">Artes cuidadosamente trabalhadas</p>
+                        <p>
+                            O jogo é uma viagem visual: Artisticamente o jogo é único e extremamente bem cuidado.
+                        </p>
+                        <img class="img-responsive img-centered" src="images/gallery/carta_juiz.jpg" alt="">
+                        <p>Se eu fosse você, compraria dois: um pra jogar e um pra iniciar uma exposição itinerante de arte.</p>
                         <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Fechar</button>
                     </div>
                 </div>
--- sections/gallery.php
+++ sections/gallery.php
@@ -22,6 +22,20 @@
                     <p class="text-muted">Para agradar coxinhas e petralhas</p>
                 </div>
             </div>
+            <div class="col-md-4 col-sm-6 portfolio-item">
+                <a href="#arteJuiz" class="portfolio-link" data-toggle="modal">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                            <i class="fa fa-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img src="images/gallery/carta_juiz-thumb.png" class="img-responsive" alt="">
+                </a>
+                <div class="portfolio-caption">
+                    <h4>Qualidade artística</h4>
+                    <p class="text-muted">Atenção no visual e na arte</p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
