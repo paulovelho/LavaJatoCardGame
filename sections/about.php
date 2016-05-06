c2d1a45b938aa495d24689a539aea107bf22e164
Paulo Henrique Martins <paulo.martins@savedo.de>
2016-05-06 12:10:46 +0200

    about

--- sections/about.php
+++ sections/about.php
@@ -9,9 +9,32 @@
         <div class="row text-center">
             <div class="col-sm-8">
                 <h4 class="service-heading">O jogo</h4>
-                <p>Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!  Pra lá , depois divoltis porris, paradis. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.</p>
-                <p>Si num tem leite então bota uma pinga aí cumpadi! Diuretics paradis num copo é motivis de denguis. Quem num gosta di mé, boa gente num é. Quem manda na minha terra sou Euzis!</p>
-                <p>Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Cevadis im ampola pa arma uma pindureta.</p>
+                <p>
+                    São tempos interessantes na fictícia ilha de Vera Cruz.
+                </p>
+                <p>
+                    O fictício governo está acuado, acusado de diversos casos de corrupção. A fictícia presidenta perde aliados políticos com a mesma freqüência que sua população perde empregos.
+                </p>
+                <p>
+                    Um juiz comanda uma operação de limpeza política cujo principal objetivo é tirar a presidenta do poder. Ambos precisam de aliados, mesmo que isso envolva se envolver com políticos criminosos.
+                </p>
+                <br/>
+                <p>
+                    O jogo da operação Lava Jato coloca você no meio dessa gostosa bagunça política. 
+                </p>
+                <p>
+                    Assuma o papel do juiz, da presidenta ou de um político que pode ser do partido da situação ou da oposição. Responda pelos seus crimes ou coloque um laranja para receber a culpa por eles. Fuja do japonês que quer te prender. Mude seu lado político para obter diferentes benefícios: Finja ser um Petralha para ter votos de confiança do governo ou finja ser um coxinha para escapar das investigações policiais.
+                </p>
+                <p>
+                    E principalmente, lute por seu objetivo: derrubar a presidenta ou mantê-la no poder.
+                </p>
+                <p>
+                    Suas ações que vão guiar este divertido jogo, pra ser jogado com toda a família e amigos. (se você ainda possuir amigos depois de jogar, lógico)
+                </p>
+                <br/>
+                <p>
+                    (Esta é uma obra de ficção. Qualquer semelhança com a realidade é pura coincidência!)
+                </p>
             </div>
             <div class="col-sm-4">
                 COLABORE!
