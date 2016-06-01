aed36f3fb5758436247e62499ed7ee319d52d433
Paulo Henrique Martins <paulovelho@paulovelho.com>
2016-06-02 00:38:14 +0200

    cards 3 on gallery

--- sections/gallery-modals.php
+++ sections/gallery-modals.php
@@ -30,7 +30,6 @@
     </div>
 </div>
 
-
 <div class="portfolio-modal modal fade" id="arteJuiz" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-content">
         <div class="close-modal" data-dismiss="modal">
@@ -51,6 +50,39 @@
                         </p>
                         <img class="img-responsive img-centered" src="images/gallery/carta_juiz.jpg" alt="">
                         <p>Se eu fosse você, compraria dois: um pra jogar e um pra iniciar uma exposição itinerante de arte.</p>
+                        <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Fechar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="portfolio-modal modal fade" id="cards3" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+                <div class="rl">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="modal-body">
+                        <h2>Novas Cartas</h2>
+                        <p class="item-intro text-muted">Continuamos trabalhando no jogo...</p>
+                        <p>
+                            A produção visual segue a toda:<br/>
+                            Estamos trabalhando no desenho das cartas e cuidando com esmero pra uma identidade visual única.
+                        </p>
+                        <p>
+                            Não só o contexto do jogo deve ser engraçado, mas o estilo das cartas também!<br/>
+                        </p>
+                        <p>
+                            <a href="regras.php">As regras</a> já estão disponíveis também!
+                        </p>
+                        <img class="img-responsive img-centered" src="images/gallery/cards-3.jpg" alt="">
                         <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Fechar</button>
                     </div>
                 </div>
