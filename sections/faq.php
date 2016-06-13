914c33c6d09ab144fe48a00c3e67c8dbce48de9b
Paulo Henrique Martins <paulo.martins@savedo.de>
2016-06-13 15:00:25 +0200

    faq content added

--- sections/faq.php
+++ sections/faq.php
@@ -11,29 +11,91 @@
             <div class="col-sm-8">
 
                 <div class="row question">
-                    <div class="col-12" onclick="toggleQuestion('p1')">
+                    <div class="col-12" onclick="toggleQuestion('p-serio')">
                         É sério isso?
                     </div>
                 </div>
                 <div class="row answer">
-                    <div class="col-12" id="p1">
-                        Lógico!
+                    <div class="col-12" id="p-serio">
+                        Lógico! Se for pra fazer gracinha eu tenho <a href="http://twitter.com/paulovelho">twitter</a>.
                     </div>
                 </div>
 
                 <div class="row question">
-                    <div class="col-12" onclick="toggleQuestion('p2')">
-                        Não é um valor muito alto?
+                    <div class="col-12" onclick="toggleQuestion('p-meta')">
+                        Não é uma meta muito ambiciosa?
                     </div>
                 </div>
                 <div class="row answer">
-                    <div class="col-12" id="p2">
-Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent A ordem dos tratores não altera o pão duris Mais vale um bebadis conhecidiss, que um alcoolatra anonimiss. Paisis, filhis, espiritis santis.<br/>
-Mé faiz elementum girarzis, nisi eros vermeio. Sapien in monti palavris qui num significa nadis i pareci latim. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</br/>
-Quem manda na minha terra sou Euzis! in elementis mé pra quem é amistosis quis leo. Pra lá , depois divoltis porris, paradis. undefined
+                    <div class="col-12" id="p-meta">
+                        É.<br/>
+                        Porém, para manter um preço acessível e um produto de qualidade, foi necessária a produção de uma quantidade mínima de baralhos. Isso custa dinheiro.<br/>
+                        81% do valor da meta é para a produção do jogo, incluindo manual, caixinha e transporte (a produção é feita na fábrica da Copag, na Zona Franca de Manaus).<br/><br/>
+                        Então, sim. É uma meta alta. Mas deixa o jogo a um preço acessível e é uma meta totalmente plausível.
                     </div>
                 </div>
 
+                <div class="row question">
+                    <div class="col-12" onclick="toggleQuestion('p-politica')">
+                        Eu preciso entender de política para jogar?
+                    </div>
+                </div>
+                <div class="row answer">
+                    <div class="col-12" id="p-politica">
+                        Não.<br/>
+                        Eu não entendo de política. A última vez que eu lembro de me atentar em uma votação, as canidatas eram Scheila Mello e Daniela Freitas.<br/>
+                        Então, ao comprar o jogo, este é o nível de patife que você estará lidando.<br/>
+                        (pelo menos eu votei na Scheila Mello, é bom deixar claro.)
+                    </div>
+                </div>
+
+                <div class="row question">
+                    <div class="col-12" onclick="toggleQuestion('p-reaca')">
+                        Achei seu jogo meio reaça.
+                    </div>
+                </div>
+                <div class="row answer">
+                    <div class="col-12" id="p-reaca">
+                        Sério? Que bom, cara.<br/>
+                        Porque teve reaça que achou o jogo meio petista. A idéia era mesmo não agradar ninguém.
+                    </div>
+                </div>
+
+                <div class="row question">
+                    <div class="col-12" onclick="toggleQuestion('p-petista')">
+                        Achei seu jogo meio petista.
+                    </div>
+                </div>
+                <div class="row answer">
+                    <div class="col-12" id="p-petista">
+                        Sério? Que bom, cara.<br/>
+                        Porque teve petista que achou o jogo meio reaça. A idéia era mesmo não agradar ninguém.
+                    </div>
+                </div>
+
+                <div class="row question">
+                    <div class="col-12" onclick="toggleQuestion('p-deprecated')">
+                        O foda é que quando esta operação acabar, o jogo vai deixar de fazer sentido, né?
+                    </div>
+                </div>
+                <div class="row answer">
+                    <div class="col-12" id="p-deprecated">
+                        Não, cara. Não...<br/>
+                        A dinâmica do jogo é independente de política. Trabalhamos bastante pra criar um negócio legal e equilibrado e você pode conferir aqui as <a href="regras.php">regras</a>.
+                    </div>
+                </div>
+
+                <div class="row question">
+                    <div class="col-12" onclick="toggleQuestion('p-gostei')">
+                        Gostei. Vou apoiar!
+                    </div>
+                </div>
+                <div class="row answer">
+                    <div class="col-12" id="p-gostei">
+                        Boa!<br/>
+                        Se eu tivesse vendido um baralho para cada vez que eu ouvisse isso, provavelmente já teria atingido a meta.
+                    </div>
+                </div>
 
             </div>
             <div class="col-sm-4">
